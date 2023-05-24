eb0eb1bb1530f66bcd4e78e839261f58593caad2
Deathdragon2003 <126626011+Deathdragon2003@users.noreply.github.com>
2023-05-24 17:46:07 +1000

    fixed some things

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Movement : MonoBehaviour
 {
 	public float speed = 1f;
     // Start is called before the first frame update
